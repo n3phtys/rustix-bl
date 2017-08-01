b5b870e92a52d630c0efc54117054c936f3496a7
nephtys <christopher.kaag@gmail.com>
2017-08-01 15:07:54 +0200

    build method for new backend moved to module

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,6 @@ pub extern crate bincode;
 #[macro_use]
 pub extern crate quick_error;
 
-use std::collections::HashSet;
-
 
 
 pub mod left_threaded_avl_tree;
@@ -33,21 +31,30 @@ pub mod errors;
 
 pub mod config;
 
-pub fn build_transient_backend() -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
-    return build_transient_backend_with(20, 20);
-}
 
-pub fn build_transient_backend_with(users_per_page: u8, top_users: u8) -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
-    return rustix_backend::RustixBackend {
-        datastore: datastore::Datastore {
-            users: Vec::new(),
-            items: Vec::new(),
-            user_id_counter: 0,
-            item_id_counter: 0,
-            categories: HashSet::new(),
-        },
-        persistencer: persistencer::TransientPersister{ events_stored: 0 },
-    };
+pub mod default {
+
+    use std::collections::HashSet;
+    use rustix_backend;
+    use datastore;
+    use persistencer;
+
+    pub fn build_transient_backend() -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
+        return build_transient_backend_with(20, 20);
+    }
+
+    pub fn build_transient_backend_with(users_per_page: u8, top_users: u8) -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
+        return rustix_backend::RustixBackend {
+            datastore: datastore::Datastore {
+                users: Vec::new(),
+                items: Vec::new(),
+                user_id_counter: 0,
+                item_id_counter: 0,
+                categories: HashSet::new(),
+            },
+            persistencer: persistencer::TransientPersister { events_stored: 0 },
+        };
+    }
 }
 
 
