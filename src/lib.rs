5d7a3b0a56b18a9c0244cd3b320e05a678ab002b
nephtys <christopher.kaag@gmail.com>
2017-08-01 15:05:11 +0200

    build method for new backend

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub extern crate bincode;
 #[macro_use]
 pub extern crate quick_error;
 
+use std::collections::HashSet;
 
 
 
@@ -32,6 +33,22 @@ pub mod errors;
 
 pub mod config;
 
+pub fn build_transient_backend() -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
+    return build_transient_backend_with(20, 20);
+}
+
+pub fn build_transient_backend_with(users_per_page: u8, top_users: u8) -> rustix_backend::RustixBackend<persistencer::TransientPersister> {
+    return rustix_backend::RustixBackend {
+        datastore: datastore::Datastore {
+            users: Vec::new(),
+            items: Vec::new(),
+            user_id_counter: 0,
+            item_id_counter: 0,
+            categories: HashSet::new(),
+        },
+        persistencer: persistencer::TransientPersister{ events_stored: 0 },
+    };
+}
 
 
 #[cfg(test)]
