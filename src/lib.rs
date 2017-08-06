674c08d198dbc0a1c70dedb6e8b0347ccd5c660e
nephtys <christopher.kaag@gmail.com>
2017-08-06 12:30:36 +0200

    moving integration tests to src package to workaround kcov issue

--- src/lib.rs
+++ src/lib.rs
@@ -72,6 +72,81 @@ pub fn build_persistent_backend(
 
 #[cfg(test)]
 mod tests {
+
+
+    extern crate tempdir;
+
+    use rustix_backend::WriteBackend;
+
+    use super::*;
+
+
     #[test]
-    fn it_works() {}
+    fn it_add_user() {
+        let dir = tempdir::TempDir::new("temptestdir").unwrap();
+
+        let b = build_persistent_backend(dir.as_ref());
+
+        println!("{:?}", b);
+
+        match b {
+            Err(_) => assert!(false),
+            Ok(mut backend) => {
+                backend.create_user("klaus".to_string());
+                assert_eq!(
+                    backend.datastore.users.get(&0u32).unwrap().username,
+                    "klaus".to_string()
+                );
+            }
+        }
+    }
+
+
+    #[test]
+    fn it_reload_added_user() {
+
+
+        /*{
+
+
+            let dir = std::path::Path::new("target");
+
+            let b = blrustix::build_persistent_backend(dir.as_ref());
+
+            println!("{:?}", b);
+
+            match b {
+                Err(_) => assert!(false),
+                Ok(mut backend) => {
+                    backend.create_user("klaus".to_string());
+                    assert_eq!(
+                        backend.datastore.users.get(&0u32).unwrap().username,
+                        "klaus".to_string()
+                    );
+                }
+            }
+
+        }*/
+
+
+
+
+        let dir = std::path::Path::new("tests/testdata");
+
+        {
+            match build_persistent_backend(dir) {
+                Err(_) => assert!(false),
+                Ok(mut backend) => {
+                    let x = backend.reload();
+                    println!("Loaded Backend: {:?}", backend);
+                    assert_eq!(x.unwrap(), 1);
+                    assert_eq!(
+                        backend.datastore.users.get(&0u32).unwrap().username,
+                        "klaus".to_string()
+                    );
+                }
+            }
+        }
+    }
+
 }
